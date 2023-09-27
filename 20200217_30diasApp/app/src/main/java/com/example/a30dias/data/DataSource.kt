f4ac25aaea706babf8e459662656b631add6b465
YeseniaTD <yeseniaandrea.torres@unmsm.edu.pe>
2023-09-27 16:55:22 -0500

    primer commit

---
+++ 20200217_30diasApp/app/src/main/java/com/example/a30dias/data/DataSource.kt
@@ -0,0 +1,40 @@
+package com.example.a30dias.data
+
+import com.example.a30dias.R
+import com.example.a30dias.model.Dias
+
+class DataSource() {
+    fun loadAffirmations(): List<Dias> {
+        return listOf<Dias>(
+            Dias(R.string.titulo1, R.drawable.image1),
+            Dias(R.string.titulo2, R.drawable.image2),
+            Dias(R.string.titulo3, R.drawable.image3),
+            Dias(R.string.titulo4, R.drawable.image4),
+            Dias(R.string.titulo5, R.drawable.image5),
+            Dias(R.string.titulo6, R.drawable.image6),
+            Dias(R.string.titulo7, R.drawable.image7),
+            Dias(R.string.titulo8, R.drawable.image8),
+            Dias(R.string.titulo9, R.drawable.image9),
+            Dias(R.string.titulo10, R.drawable.image10),
+            Dias(R.string.titulo11, R.drawable.image1),
+            Dias(R.string.titulo12, R.drawable.image2),
+            Dias(R.string.titulo13, R.drawable.image3),
+            Dias(R.string.titulo14, R.drawable.image4),
+            Dias(R.string.titulo15, R.drawable.image5),
+            Dias(R.string.titulo16, R.drawable.image6),
+            Dias(R.string.titulo17, R.drawable.image7),
+            Dias(R.string.titulo18, R.drawable.image8),
+            Dias(R.string.titulo19, R.drawable.image9),
+            Dias(R.string.titulo20, R.drawable.image10),
+            Dias(R.string.titulo21, R.drawable.image1),
+            Dias(R.string.titulo22, R.drawable.image2),
+            Dias(R.string.titulo23, R.drawable.image3),
+            Dias(R.string.titulo24, R.drawable.image4),
+            Dias(R.string.titulo25, R.drawable.image5),
+            Dias(R.string.titulo26, R.drawable.image6),
+            Dias(R.string.titulo27, R.drawable.image7),
+            Dias(R.string.titulo28, R.drawable.image8),
+            Dias(R.string.titulo29, R.drawable.image9),
+            Dias(R.string.titulo30, R.drawable.image10))
+    }
+}
---
+++ 20200217_30diasApp/app/src/main/java/com/example/a30dias/model/Dias.kt
@@ -0,0 +1,9 @@
+package com.example.a30dias.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Dias(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
